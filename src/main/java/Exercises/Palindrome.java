e94fc4a3f58d29d4b0701bf0efef5072e2369894
KaczmarekIgor <Igor009@interia.eu>
2020-02-03 12:19:37 +0100

    Palindrome for single word

--- src/main/java/Exercises/Palindrome.java
+++ src/main/java/Exercises/Palindrome.java
@@ -1,4 +1,27 @@
 package Exercises;
 
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String x = scanner.nextLine();
+        int j = x.length();
+        int w = 0; // 1 slowo
+        int y = (j - 1); // ostatnie slowo
+
+        for (int i = 0; i < j; i++) { // wypelnia
+            if (x.charAt(i) != x.charAt(y)) { // zwraca znak
+                w = 1;
+                break;
+            }
+            y--;
+
+        }
+        if (w == 1) {
+            System.out.println("nie jest");
+        } else {
+            System.out.println("Jest");
+        }
+    }
 }
